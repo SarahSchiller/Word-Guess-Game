545ed890c4384a148f32e5f51d06479f4ebe82d9
Sarah Schiller <sarahschiller@Sarahs-MBP.attlocal.net>
2018-08-16 10:27:26 -0500

    initial site files

---
+++ assets/javascript/game.js
@@ -0,0 +1,7 @@
+let dictionary = ["beets", "collar blind", "dinkin\'\ flicka", "dundies","new guy", "business bitch", "bandit", "merger", "paper", "bobblehead", "escape goat", "fleece it out", "all-inclusive";
+
+
+let word = dictionary[Math.floor(Math.random() * dicitionary.length)];
+
+
+document.getElementsByClassName("word-choice").innerHTML = word; 
